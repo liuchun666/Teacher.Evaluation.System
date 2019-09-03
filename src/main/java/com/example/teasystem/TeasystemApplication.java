24189e9df194f0881ee8fdfd066b750bdc4ef9f8
xiongsihao9542 <xiongsihaov587@qq.com>
2019-09-03 16:10:16 +0800

    系统框架

---
+++ src/main/java/com/example/teasystem/TeasystemApplication.java
@@ -0,0 +1,17 @@
+package com.example.teasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/****
+ * 教师评选系统
+ */
+
+@SpringBootApplication
+public class TeasystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeasystemApplication.class, args);
+    }
+
+}
