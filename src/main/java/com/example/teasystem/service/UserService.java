c060ee29d18624bf12808b45c718d9d66387d5d5
liu55510 <1534282383@qq.com>
2019-09-18 11:46:53 +0800

    Merge branch 'master' of https://github.com/liuchun666/Teacher.Evaluation.System
    # Conflicts:
    #	src/main/java/com/example/teasystem/entity/Role.java
    #	src/main/java/com/example/teasystem/entity/User.java
    #	src/main/resources/templates/manage/index.html

---
+++ src/main/java/com/example/teasystem/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.teasystem.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface UserService {
+    /**
+     * 读取excel中的数据,生成list
+     */
+    String readExcelFile( MultipartFile file);
+}
