29275fc4d09294d18d019c9d6ea16d211526a5ac
liu55510 <1534282383@qq.com>
2019-09-19 19:12:19 +0800

    Merge branch 'master' of https://github.com/liuchun666/Teacher.Evaluation.System
    # Conflicts:
    #	src/main/java/com/example/teasystem/controller/IndexController.java
    #	src/main/resources/templates/manage/index.html

--- src/main/java/com/example/teasystem/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/teasystem/service/impl/UserServiceImpl.java
@@ -15,28 +15,46 @@ import java.util.ArrayList;
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    AdminMapper usermap;
+    AdminMapper adminMapper;
     @Override
     public ArrayList<User> selectUser(String account, String password) {
-        ArrayList<User> users = usermap.selectUser(account, password);
+        ArrayList<User> users = adminMapper.selectUser(account, password);
         return  users;
     }
 
     @Override
     public ArrayList<UserRole> selectUserRole(String userId) {
-        ArrayList<UserRole> userRoles = usermap.selectUserRole(userId);
+        ArrayList<UserRole> userRoles = adminMapper.selectUserRole(userId);
         return userRoles;
     }
 
     @Override
     public ArrayList<Permission> selectPermission(String roleId) {
-        ArrayList<Permission> Permissions = usermap.selectPermission(roleId);
+        ArrayList<Permission> Permissions = adminMapper.selectPermission(roleId);
         return Permissions;
     }
 
     @Override
     public ArrayList<Permission> selectPermissionSon(String perId) {
-        ArrayList<Permission> Permissions = usermap.selectPermissionSon(perId);
+        ArrayList<Permission> Permissions = adminMapper.selectPermissionSon(perId);
         return null;
     }
+    @Override
+    public int addUser(User user) {
+        int i = adminMapper.addUser(user);
+        return 0;
+    }
+
+    @Override
+    public int deleteUser(String account) {
+        int i=adminMapper.deleteUser(account);
+        return i;
+    }
+
+    @Override
+    public int updateUser(User user) {
+        int i= adminMapper.updateUser(user);
+        return i;
+    }
+
 }
